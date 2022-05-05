e273e1d8b9da80472445dd237e0b6020aa6b2caa
Rob <r.verheyen@ucl.ac.uk>
2022-05-05 10:18:21 +0100

    Update data download script

--- experiments/4gluino/download_data.sh
+++ experiments/4gluino/download_data.sh
@@ -1,2 +1,2 @@
-wget -O data_2_gluino.csv https://zenodo.org/record/5220652/files/data_2_gluino.csv
-wget -O data_4_gluino.csv https://zenodo.org/record/5220652/files/data_4_gluino.csv
+wget -O data_2_gluino.csv https://zenodo.org/record/6519993/files/data_2_gluino.csv
+wget -O data_4_gluino.csv https://zenodo.org/record/6519993/files/data_4_gluino.csv
